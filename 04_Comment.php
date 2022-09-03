2f91c997ea410488d3d034d9d9749fdfb79b8bb4
krushna Gavali <krushnagawali744@gmail.com>
2022-09-03 11:26:52 -0100

    Create 04_Comment.php

---
+++ 04_Comment.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <?php
+        echo "Single line comment" . "<br>";
+        // this is single line comment
+
+        echo "single line comment 2nd way" . "<br>";
+         # this is also single line comment
+
+        echo "multipal line comment" . "<br>";
+        /* This is right 
+            here is the 
+            multapal line 
+            comment , that is hiding 
+            multipal lines 
+        */
+
+
+    ?>
+<body>
+    
+</body>
+</html>

81eef832aa9779e1ffd363347fa398bbcaf45b24
krushna Gavali <krushnagawali744@gmail.com>
2022-09-03 11:26:03 -0100

    Create 03_Calculator.php

---
+++ 03_Calculator.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <h1>calculator program using php</h1>
+    </head>
+<body>
+    <?php
+        $number1 = 34;
+        $number2 = 2;
+        $add = $number1 + $number2;
+        $div = $number1 / $number2;
+        $mul = $number1 * $number2;
+        $sum = $number1 - $number2;
+            echo "<br>";
+        echo "add is :" . $add ."<br>";
+        echo "div is :" . $div ."<br>";
+        echo "Mul is :" . $mul ."<br>";
+        echo "sum is :" . $sum;
+    ?>
+</body>
+</html>

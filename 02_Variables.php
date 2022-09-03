69ba62ad5f03f6ae5741f126eb1a6637f12309a5
krushna Gavali <krushnagawali744@gmail.com>
2022-09-03 11:23:49 -0100

    Create 02_Variables.php

---
+++ 02_Variables.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <h1>Variables in php</h1>
+    </head>
+<body>
+    <?php
+        $var = "Suresh" ;
+        $var2 = "Ranesh";
+        $var3 = "Mukesh";
+        $var4 = "Rakesh";
+
+        echo "evaey movie starts from ad" . $var . $var2 . $var3 . $var4 ;
+    ?>
+</body>
+</html>

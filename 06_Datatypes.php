428c1f9b69972ff3d2ccd8b7d66c25a37c5f99b3
krushna Gavali <krushnagawali744@gmail.com>
2022-10-17 17:34:07 +0530

    Update 06_Datatypes.php

--- 06_Datatypes.php
+++ 06_Datatypes.php
@@ -6,4 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+    
 <body>
+    <?php
+        // Data types in php
+            #-- string  
+            #-- integer
+            #-- float
+            #-- boolean
+            #-- object
+            #-- array

3b7311f8065068803cf0cf3f799e320518bac7b9
krushna Gavali <krushnagawali744@gmail.com>
2022-10-18 21:46:53 +0530

    Update 07_Basics.php

--- 07_Basics.php
+++ 07_Basics.php
@@ -7,3 +7,18 @@
     <title>Document</title>
 </head>
         <style>
+                *{margin: 0 ;
+                    padding: 0;
+                    box-sizing: border-box ;
+                
+                }
+
+                .container{
+                    max-width: 80;
+                    background-color: gray ;
+                    margin: auto;
+                    padding: 23px;
+                }
+
+        </style>
+            

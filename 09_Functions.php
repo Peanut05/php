f7eb8ec9fd8df8a55c85f2bddf9ac343a51d1327
krushna Gavali <krushnagawali744@gmail.com>
2022-10-21 17:20:45 +0530

    Update 09_Functions.php

--- 09_Functions.php
+++ 09_Functions.php
@@ -6,3 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<body>
+    <?php
+        function printh(){
+            echo "<br> Hello";
+        }
+        printh();
+        printh();
+        printh();
+        printh();

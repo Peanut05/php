31b0b75935ddcfb0fede2c0123da1eeb04d4ea0d
krushna Gavali <krushnagawali744@gmail.com>
2022-10-31 17:52:40 +0530

    Update 12_New_Pro_Cmt.php

--- 12_New_Pro_Cmt.php
+++ 12_New_Pro_Cmt.php
@@ -39,3 +39,20 @@ if(isset($_POST['name'])){
     $con->close();
 }
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Form </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="back">
+    <div class="container">
+        <h1>Welcome to Peanut's travel Agency</h1>
+        <p>
+            Welcome To Peanut's Travel Agency 
+            fill your detials for travel with us
+
+        </p>
